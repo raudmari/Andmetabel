7a54709db956afc8a8453d41bf5b8df1b834d45e
Marion <marion@marion.ee>
2021-11-25 09:34:06 +0200

    Second commit

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const myfuncs = require("./helpers/myfuncs.js");
 const app = express();
 
 jsonData = jsonData.objects.sort((a, b) => a.Lastname > b.Lastname && 1 || -1)
-console.log(jsonData);
 
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + '/public'));
@@ -26,37 +25,37 @@ app.get("/", (req, res) => {
 });
 
 app.get("/:id", (req, res) => {
-      const {
-        id
-      } = req.params;
-      let find = false;
-      for (var jd of jsonData) {
-        if (jd.ID === id) {
-          find = true;
-          const data = jsonData.find(jd => jd.ID === id);
-          title = "Show data";
-          res.render("details", {
-            title,
-            myfuncs,
-            data
-          });
-        }
-      }
-      if (find == false) {
-        title = "Error 404",
-          res.render("Error404", {
-            title
-          });
-      }
-    });
-
-    app.listen(3000, () => {
-      console.log("Server is running on port " + 3000);
-    });
-
-    app.get("/*", (req, res) => {
-      title = "Error 404";
+  const {
+    id
+  } = req.params;
+  let find = false;
+  for (var jd of jsonData) {
+    if (jd.ID === id) {
+      find = true;
+      const data = jsonData.find(jd => jd.ID === id);
+      title = "Show data";
+      res.render("details", {
+        title,
+        myfuncs,
+        data
+      });
+    }
+  }
+  if (find == false) {
+    title = "Error 404",
       res.render("Error404", {
         title
       });
-    });
+  }
+});
+
+app.get("/*", (req, res) => {
+  title = "Error 404";
+  res.render("Error404", {
+    title
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port " + 3000);
+});
